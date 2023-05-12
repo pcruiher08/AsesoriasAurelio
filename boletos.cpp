da6538b95a3c6d498c65f9f5855e1b047a98929a
pcruiher08 <pcruiher089@gmail.com>
2023-05-11 18:04:35 -0600

    first

---
+++ boletos.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <math.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+
+int cantidadDeCiudades = 3; 
+//arreglo estatico de ciudades
+string ciudades[] = {"CDMX", "Monterrey", "Guadalajara"};
+
+cout<<"Imprimiendo el arreglo de ciudades"<<endl;
+for(int i = 0; i < cantidadDeCiudades; i++){
+    cout<<ciudades[i]<<endl;
+}
+
+
+
+
+int cantidadDePaises = 4;
+
+string paises[cantidadDePaises];
+
+paises[0] = "Alemania";
+paises[1] = "Mexico";
+paises[2] = "Canada";
+paises[3] = "Argentina";
+
+
+cout<<"Imprimiendo el arreglo de paises"<<endl;
+for(int i = 0; i < cantidadDePaises; i++){
+    cout<<paises[i]<<endl;
+}
+
+return 0;
+}
