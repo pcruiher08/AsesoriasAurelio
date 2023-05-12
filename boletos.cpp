f2dde7d295abf8aab47b52ed69792c5ac66c45d4
pcruiher08 <pcruiher089@gmail.com>
2023-05-11 18:18:28 -0600

    ejemplo pares

--- boletos.cpp
+++ boletos.cpp
@@ -20,6 +20,8 @@ for(int i = 0; i < cantidadDeCiudades; i++){
 
 
 
+//el primer elemento del par es de tipo string, y el segundo de tipo int
+
 
 int cantidadDePaises = 4;
 
@@ -35,6 +37,28 @@ paises[3] = "Argentina";
 cout<<"Imprimiendo el arreglo de paises"<<endl;
 for(int i = 0; i < cantidadDePaises; i++){
     cout<<paises[i]<<endl;
+}
+
+
+
+//se declara un arreglo de tipo pair<string, int> para poder guardar pares (o tuplas)
+pair<string, int> datosboletos[] = {{"CDMX", 15}, {"Monterrey", 12}, {"Guadalajara", 0}};
+
+//para recorrerlo es igual, pero se usan las palabras reservadas first y second para acceder al primer y segundo elemento del par respectivamente
+for(int i = 0; i < cantidadDeCiudades; i++){
+    cout<<"La ciudad es: " << datosboletos[i].first << " y los boletos que hay son: "<< datosboletos[i].second<<endl;
+}
+
+//tambien se puede acceder al arreglo para hacer modificaciones
+//por ejemplo para decir que se vendieron otros cinco boletos para CDMX seria
+
+
+datosboletos[0].second += 5;
+
+//entonces se imprime de la siguiente manera
+cout<<"Arreglo modificado"<<endl;
+for(int i = 0; i < cantidadDeCiudades; i++){
+    cout<<"La ciudad es: " << datosboletos[i].first << " y los boletos que hay son: "<< datosboletos[i].second<<endl;
 }
 
 return 0;
